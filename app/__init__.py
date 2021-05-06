f2f15f6800ed31fb82f52eac1ea276c211b5de72
Francesco Milani <fmilani@bink.com>
2021-05-06 11:40:44 +0300

    add api init for healthz endpoints

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
+
+from app.api.healthz import healthz_router
+from app.core.config import settings
+from app.core.exception_handlers import http_exception_handler, request_validation_handler
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_PREFIX}/openapi.json")
+    app.include_router(healthz_router)
+
+    app.add_exception_handler(RequestValidationError, request_validation_handler)
+    app.add_exception_handler(HTTPException, http_exception_handler)
+
+    return app
