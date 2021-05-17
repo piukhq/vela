4d1445d7a79329f774f8d0b42af3e038a567e105
Stewart Perrygrove <sperrygrove@bink.com>
2021-05-17 17:57:36 +0100

    BPL-69 Add earn rule model/table

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,8 +1,22 @@
 # mypy checks for sqlalchemy core 2.0 require sqlalchemy2-stubs
-from sqlalchemy import Column, Integer
-from sqlalchemy.orm import as_declarative  # type: ignore
+from sqlalchemy import Column, DateTime, Integer, text
+from sqlalchemy.orm import as_declarative, declarative_mixin  # type: ignore
 
 
 @as_declarative()
 class Base:
     id = Column(Integer, primary_key=True, index=True)
+
+
+utc_timestamp_sql = text("TIMEZONE('utc', CURRENT_TIMESTAMP)")
+
+
+@declarative_mixin
+class TimestampMixin:
+    created_at = Column(DateTime, server_default=utc_timestamp_sql, nullable=False)
+    updated_at = Column(
+        DateTime,
+        server_default=utc_timestamp_sql,
+        onupdate=utc_timestamp_sql,
+        nullable=False,
+    )
