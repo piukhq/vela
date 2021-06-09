3cbdc3cb2d02ad4e5144183ac6f1d8827051e420
Francesco Milani <fmilani@bink.com>
2021-06-09 12:28:20 +0300

    BPL-156 transaction total as int

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -6,15 +6,10 @@ from pydantic.types import UUID4
 
 class CreateTransactionSchema(BaseModel):  # pragma: no cover
     transaction_id: str = Field(..., alias="id")
-    amount: float = Field(..., alias="transaction_total")
+    amount: int = Field(..., alias="transaction_total")
     datetime: int
     mid: str = Field(..., alias="MID")
     account_holder_uuid: UUID4 = Field(..., alias="loyalty_id")
-
-    @validator("amount")
-    @classmethod
-    def get_float(cls, v: float) -> int:
-        return int(v * 100)
 
     @validator("datetime")
     @classmethod
