7b26394fc2393de39f773a88914fdf43a7281816
Francesco Milani <fmilani@bink.com>
2021-06-09 12:37:31 +0300

    BPL-156 use strict int

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -1,12 +1,12 @@
 from datetime import datetime as dt
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, StrictInt, validator
 from pydantic.types import UUID4
 
 
 class CreateTransactionSchema(BaseModel):  # pragma: no cover
     transaction_id: str = Field(..., alias="id")
-    amount: int = Field(..., alias="transaction_total")
+    amount: StrictInt = Field(..., alias="transaction_total")
     datetime: int
     mid: str = Field(..., alias="MID")
     account_holder_uuid: UUID4 = Field(..., alias="loyalty_id")
