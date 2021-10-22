baa7e5d740130cacae095ce50c565daed638198b
David Felce <dfelce@bink.com>
2021-10-22 13:04:46 +0100

    BPL-283: removed commented block

--- app/crud/campaign.py
+++ app/crud/campaign.py
@@ -26,33 +26,3 @@ async def get_campaigns_by_slug(
         )
 
     return await async_run_query(_query, db_session, rollback_on_exc=False)
-
-
-# async def get_active_campaign_slugs(
-#     db_session: "AsyncSession", retailer: RetailerRewards, transaction_time: datetime = None
-# ) -> List[str]:
-#     async def _query() -> list:
-#         return (
-#             await db_session.execute(
-#                 select(Campaign.slug, Campaign.start_date, Campaign.end_date).filter_by(
-#                     retailer_id=retailer.id, status=CampaignStatuses.ACTIVE
-#                 )
-#             )
-#         ).all()
-#
-#     campaign_rows = await async_run_query(_query, db_session, rollback_on_exc=False)
-#
-#     if transaction_time is not None:
-#         valid_campaigns = [
-#             slug
-#             for slug, start, end in campaign_rows
-#             if start <= transaction_time and (end is None or end > transaction_time)
-#         ]
-#
-#     else:
-#         valid_campaigns = [row[0] for row in campaign_rows]
-#
-#     if not valid_campaigns:
-#         raise HttpErrors.NO_ACTIVE_CAMPAIGNS.value
-#
-#     return valid_campaigns
