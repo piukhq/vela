3a0952fc562d717220c9ecb17dfb6d37cbdde2aa
Francesco Milani <fmilani@bink.com>
2022-07-13 11:53:13 +0300

    BPL-607 update tx history migration max txs logic

--- tx_history_migration/cli.py
+++ tx_history_migration/cli.py
@@ -5,49 +5,44 @@ from .processed_txs_handler import VelaTxHistoryHandler
 
 cli = Typer(name="Transaction History Migration", no_args_is_help=True)
 
-# pylint: disable=too-complex
+# pylint: disable=too-complex,too-many-locals
 @cli.command(no_args_is_help=True)
 def run(
     retailer_slug: str = Argument(...),
     polaris_db_name: str = Option("polaris"),
     batch_size: int = Option(1000, min=10, max=5000),
     debug: bool = Option(False, help="If set to True logs the activity payload instead of sending it."),
-    max_txs_to_process: int = Option(
-        None,
-        min=1,
-        help=(
-            "Instead of processing all transactions in batches, "
-            "fetches and processes the provided number of transactions in a single batch then stops."
-        ),
-    ),
+    max_txs_to_process: int = Option(None, min=1, help="Maximum number of transactions to process."),
 ) -> None:
     echo("Starting script...")
+
     current_batch = 0  # pylint: disable=invalid-name
     existing_history_txs_ids = get_existing_history_txs_ids(polaris_db_name, retailer_slug)
 
-    if max_txs_to_process:
-        batch_size = max_txs_to_process
-
-    with VelaTxHistoryHandler(batch_size, retailer_slug, existing_history_txs_ids, debug=debug) as handler:
+    with VelaTxHistoryHandler(retailer_slug, existing_history_txs_ids, debug=debug) as handler:
         tx_to_process_count = handler.get_processed_tx_count()
 
-        msg = f"{tx_to_process_count:d} transactions to process, will process "
+        msg = f"{tx_to_process_count:d} transactions to process, will process in batches of {batch_size:d}"
+
+        tot_tx_to_process = tx_to_process_count
         if max_txs_to_process:
-            msg += f"up to {max_txs_to_process:d} transactions and stop."
-            estimated_tot_batches = 1
+            msg += f" up to {max_txs_to_process:d} transactions."
 
-        else:
-            msg += f"in batches of {handler.batch_size:d}"
-            estimated_tot_batches = tx_to_process_count // handler.batch_size
+            if max_txs_to_process < tx_to_process_count:
+                tot_tx_to_process = max_txs_to_process
 
-            if tx_to_process_count % handler.batch_size != 0:
-                estimated_tot_batches += 1
+        remaining_txs_n = tot_tx_to_process
+        estimated_tot_batches = tot_tx_to_process // batch_size
+        if tot_tx_to_process % batch_size != 0:
+            estimated_tot_batches += 1
 
         echo(msg)
         try:
             while True:
                 current_batch += 1
-                processed_txs_data = handler.fetch_batch()
+                next_batch_size = batch_size if remaining_txs_n >= batch_size else remaining_txs_n
+
+                processed_txs_data = handler.fetch_batch(next_batch_size)
                 if not processed_txs_data:
                     break
 
@@ -55,7 +50,8 @@ def run(
                 for tx_data in processed_txs_data:
                     handler.produce_activity(tx_data)
 
-                if max_txs_to_process:
+                remaining_txs_n -= next_batch_size
+                if remaining_txs_n <= 0:
                     break
 
         except KeyboardInterrupt:
