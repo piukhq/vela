94567e565f9968f368368d3277b35e4ab57bebd4
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-12 08:10:22 +0100

    add application name to db

--- app/db/session.py
+++ app/db/session.py
@@ -10,13 +10,20 @@ if settings.USE_NULL_POOL or settings.TESTING:
 else:
     null_pool = {}  # pragma: no cover
 
+# application name
+CONNECT_ARGS = {"application_name": "vela"}
 
 # future=True enables sqlalchemy core 2.0
 async_engine = create_async_engine(
     settings.SQLALCHEMY_DATABASE_URI_ASYNC, pool_pre_ping=True, future=True, echo=settings.SQL_DEBUG, **null_pool
 )
 sync_engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, echo=settings.SQL_DEBUG, future=True, **null_pool
+    settings.SQLALCHEMY_DATABASE_URI,
+    connect_args=CONNECT_ARGS,
+    pool_pre_ping=True,
+    echo=settings.SQL_DEBUG,
+    future=True,
+    **null_pool
 )
 AsyncSessionMaker = sessionmaker(bind=async_engine, future=True, expire_on_commit=False, class_=AsyncSession)
 SyncSessionMaker = sessionmaker(bind=sync_engine, future=True, expire_on_commit=False)
