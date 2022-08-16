6ac50b7ef2b86b3a95ac353f1c09ebcc1029519b
Haffi Mazhar <hmazhar@bink.com>
2022-08-16 11:44:46 +0100

    BPL-670: Process-refunds-with-max-amount - Alter get_active_campaign_slugs to get_active_campaigns

--- app/crud/retailer.py
+++ app/crud/retailer.py
@@ -4,7 +4,7 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload
 
 from app.db.base_class import async_run_query
-from app.enums import CampaignStatuses, HttpErrors, LoyaltyTypes, TransactionProcessingStatuses
+from app.enums import CampaignStatuses, HttpErrors, TransactionProcessingStatuses
 from app.models import Campaign, EarnRule, RetailerRewards, Transaction
 from app.models.retailer import RetailerStore
 
@@ -26,93 +26,54 @@ async def get_retailer_by_slug(db_session: "AsyncSession", retailer_slug: str) -
     return retailer
 
 
-async def get_active_campaign_slugs(
-    db_session: "AsyncSession", retailer: RetailerRewards, transaction: Transaction = None
-) -> list[str]:
+async def get_active_campaigns(
+    db_session: "AsyncSession", retailer: RetailerRewards, transaction: Transaction = None, join_rules: bool = False
+) -> list[Campaign]:
+
+    opt = [joinedload(Campaign.earn_rules), joinedload(Campaign.reward_rule)] if join_rules else []
+
     async def _query() -> list:
         return (
-            await db_session.execute(
-                select(Campaign.slug, Campaign.start_date, Campaign.end_date).filter_by(
-                    retailer_id=retailer.id, status=CampaignStatuses.ACTIVE
+            (
+                await db_session.execute(
+                    select(Campaign)
+                    .options(*opt)
+                    .where(Campaign.retailer_id == retailer.id, Campaign.status == CampaignStatuses.ACTIVE)
                 )
             )
-        ).all()
+            .unique()
+            .scalars()
+            .all()
+        )
 
-    campaign_rows = await async_run_query(_query, db_session, rollback_on_exc=False)
+    campaigns = await async_run_query(_query, db_session, rollback_on_exc=False)
 
-    if transaction is not None:
-        valid_campaigns = [
-            slug
-            for slug, start, end in campaign_rows
-            if start <= transaction.datetime and (end is None or end > transaction.datetime)
+    campaigns = (
+        [
+            campaign
+            for campaign in campaigns
+            if campaign.start_date <= transaction.datetime
+            and (campaign.end_date is None or campaign.end_date > transaction.datetime)
         ]
+        if transaction is not None
+        else campaigns
+    )
 
-    else:
-        valid_campaigns = [row[0] for row in campaign_rows]
+    if not campaigns:
+        if transaction:
 
-    async def _update_tx_status(*, tx: Transaction) -> None:
-        tx.status = TransactionProcessingStatuses.NO_ACTIVE_CAMPAIGNS
-        await db_session.commit()
+            async def _update_tx_status(*, tx: Transaction) -> None:
+                tx.status = TransactionProcessingStatuses.NO_ACTIVE_CAMPAIGNS
+                await db_session.commit()
 
-    if not valid_campaigns:
-        if transaction is not None:
             await async_run_query(_update_tx_status, db_session, tx=transaction)
 
         raise HttpErrors.NO_ACTIVE_CAMPAIGNS.value
 
-    return valid_campaigns
+    return campaigns
 
 
-def _calculate_amount_and_set_threshold(
-    adjustment_amounts: dict[str, dict], tx_amount: int, campaign: Campaign, earn_rule: EarnRule
-) -> None:
-
-    # NOTE: Business logic mandates that the earn rules of a campaign must have the same threshold.
-    # in case of discrepacies we set the threshold to the lowest af all thresholds.
-    if adjustment_amounts[campaign.slug]["threshold"]:
-        adjustment_amounts[campaign.slug]["threshold"] = min(
-            adjustment_amounts[campaign.slug]["threshold"], earn_rule.threshold
-        )
-    else:
-        adjustment_amounts[campaign.slug]["threshold"] = earn_rule.threshold
-
-    if campaign.loyalty_type == LoyaltyTypes.ACCUMULATOR:
-
-        if earn_rule.max_amount and tx_amount > earn_rule.max_amount:
-            adjustment_amounts[campaign.slug]["amount"] += earn_rule.max_amount
-            adjustment_amounts[campaign.slug]["accepted"] = True
-
-        # pylint: disable=chained-comparison
-        elif (tx_amount < 0 and campaign.reward_rule.allocation_window > 0) or tx_amount >= earn_rule.threshold:
-            adjustment_amounts[campaign.slug]["amount"] += tx_amount * earn_rule.increment_multiplier
-            adjustment_amounts[campaign.slug]["accepted"] = True
-
-    elif campaign.loyalty_type == LoyaltyTypes.STAMPS and tx_amount >= earn_rule.threshold:
-        adjustment_amounts[campaign.slug]["amount"] += earn_rule.increment * earn_rule.increment_multiplier
-        adjustment_amounts[campaign.slug]["accepted"] = True
-
-
-def _calculate_transaction_amounts_for_each_earn_rule(campaigns: list[Campaign], tx_amount: int) -> dict:
-    adjustment_amounts: dict[str, dict] = {}
-
-    # pylint: disable=chained-comparison
-    for campaign in campaigns:
-        adjustment_amounts[campaign.slug] = {
-            "type": campaign.loyalty_type,
-            "amount": 0,
-            "threshold": None,
-            "accepted": False,
-        }
-
-        for earn_rule in campaign.earn_rules:
-            _calculate_amount_and_set_threshold(adjustment_amounts, tx_amount, campaign, earn_rule)
-
-    return adjustment_amounts
-
-
-async def get_adjustment_amounts(
-    db_session: "AsyncSession", transaction: Transaction, campaign_slugs: list[str]
-) -> dict:
+async def get_campaign_with_rules(db_session: "AsyncSession", campaign_slugs: list[str]) -> list[Campaign]:
     async def _query() -> list[EarnRule]:
         return (
             (
@@ -127,8 +88,7 @@ async def get_adjustment_amounts(
             .all()
         )
 
-    campaigns = await async_run_query(_query, db_session, rollback_on_exc=False)
-    return _calculate_transaction_amounts_for_each_earn_rule(campaigns, transaction.amount)
+    return await async_run_query(_query, db_session, rollback_on_exc=False)
 
 
 async def get_retailer_store_name_by_mid(db_session: "AsyncSession", retailer_id: int, mid: str) -> str | None:
