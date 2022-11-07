57dc38aef7360f2a2295444d28f70ddbe4f13716
Francesco Milani <fmilani@bink.com>
2022-11-07 17:23:58 +0200

    BPL-281 transaction error fields

--- vela/core/exception_handlers.py
+++ vela/core/exception_handlers.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import cast
+from typing import TYPE_CHECKING
 
 from fastapi import Request, Response
 from fastapi.exceptions import RequestValidationError
@@ -8,10 +8,17 @@ from fastapi.responses import UJSONResponse
 from starlette.exceptions import HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_500_INTERNAL_SERVER_ERROR
 
+if TYPE_CHECKING:
+    from pydantic.error_wrappers import ErrorDict
+
+
 logger = logging.getLogger(__name__)
 
 
-def _format_validation_errors(payload: list[dict]) -> tuple[int, list[dict] | dict]:  # pragma: no cover
+def _format_validation_errors(
+    url_path: str, payload: list["ErrorDict"]
+) -> tuple[int, list[dict] | dict]:  # pragma: no cover
+    fields = []
     for error in payload:
         if error["type"] == "value_error.jsondecode":
             return (
@@ -19,17 +26,25 @@ def _format_validation_errors(payload: list[dict]) -> tuple[int, list[dict] | di
                 {"display_message": "Malformed request.", "code": "MALFORMED_REQUEST"},
             )
 
-    return (
-        HTTP_422_UNPROCESSABLE_ENTITY,
-        {"display_message": "BPL Schema not matched.", "code": "INVALID_CONTENT"},
-    )
+        fields.append(error["loc"][-1])
+
+    if "/transaction" in url_path:
+        content = {
+            "display_message": "Submitted fields are missing or invalid.",
+            "code": "FIELD_VALIDATION_ERROR",
+            "fields": fields,
+        }
+    else:
+        content = {"display_message": "BPL Schema not matched.", "code": "INVALID_CONTENT"}
+
+    return HTTP_422_UNPROCESSABLE_ENTITY, content
 
 
 # customise Api RequestValidationError
 async def request_validation_handler(
     request: Request, exc: RequestValidationError  # pylint: disable=unused-argument
 ) -> Response:
-    status_code, content = _format_validation_errors(cast(list[dict], exc.errors()))
+    status_code, content = _format_validation_errors(request.url.path, exc.errors())
     return UJSONResponse(status_code=status_code, content=content)
 
 
@@ -39,7 +54,7 @@ async def http_exception_handler(
 ) -> UJSONResponse:
 
     if exc.status_code == HTTP_422_UNPROCESSABLE_ENTITY and isinstance(exc.detail, list):
-        status_code, content = _format_validation_errors(exc.detail)
+        status_code, content = _format_validation_errors(request.url.path, exc.detail)
     else:
         status_code, content = exc.status_code, exc.detail
 
